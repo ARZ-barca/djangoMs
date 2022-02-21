3a69b6872cf12edeccf4cd97bf191783c8a5b813
alireza <alireza.zabihi@yahoo.com>
2022-02-21 12:31:47 +0330

    templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,11 +1,32 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'alireza z',
+        'title': 'first post',
+        'content': 'first post content',
+        'date': 'mehr 1400'
+    },
+    {
+        'author': 'amir z',
+        'title': 'second post',
+        'content': 'second post content',
+        'date': 'azar 1400'
+    }
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': about})
+
