3c9d7b977bd7f6780e1ae7113842b8766483ed7c
alireza <alireza.zabihi@yahoo.com>
2022-02-26 08:46:21 +0330

    shell

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -6,3 +6,5 @@ urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about,  name='blog-about'),
 ]
+
+
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,28 +1,30 @@
+import imp
 from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'alireza z',
-        'title': 'first post',
-        'content': 'first post content',
-        'date': 'mehr 1400'
-    },
-    {
-        'author': 'amir z',
-        'title': 'second post',
-        'content': 'second post content',
-        'date': 'azar 1400'
-    }
-]
+# posts = [
+#     {
+#         'author': 'alireza z',
+#         'title': 'first post',
+#         'content': 'first post content',
+#         'date_posted': 'mehr 1400'
+#     },
+#     {
+#         'author': 'amir z',
+#         'title': 'second post',
+#         'content': 'second post content',
+#         'date_posted': 'azar 1400'
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
